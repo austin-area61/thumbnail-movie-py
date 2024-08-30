904511f86ca9b4bbb37d0b839180c540841bac6d
austin-area61 <austinonyango.area61@gmail.com>
2024-08-30 20:51:51 +0300

    using CompositeVideoClip() function that merges multiple clips (video and images) into a single video, layering them according to their start times.

--- thumb-nail.py
+++ thumb-nail.py
@@ -7,4 +7,9 @@ thumbnail = ImageClip("")
 ##Set the thumbnail duration
 thumbnail = thumbnail.set_duration(1)
 
-thumbnail = thumbnail.resize(width=video.w)
+thumbnail = thumbnail.resize(width=video.w) #aspect ratio
+thumbnail = thumbnail.set_start(0)
+
+final_clip = CompositeVideoClip([thumbnail, video.set_start(thumbnail.duration)]) #uses CompositeVideoClip()function that merges multiple clips (video and images) into a single video, layering them according to their start times
+
+final_clip.write_videofile("output_video_with_thumbnail.mp4", codec="libx264") 
