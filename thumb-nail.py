8e54768e7722ad022a11819cfd2b4ced5abb6a7c
austin-area61 <austinonyango.area61@gmail.com>
2024-08-30 21:26:21 +0300

    adjusted the code in resize.py file in the source file affecting the adjustin call to fix an error

--- thumb-nail.py
+++ thumb-nail.py
@@ -1,15 +1,15 @@
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 
-#import video and image for thumbnail
-video = VideoFileClip("C:\Users\Austin\C:\Users\AUSTIN\thumbnail-movie-py\spidervswaspvideo.mp4")
-thumbnail = ImageClip("C:\Users\Austin\C:\Users\AUSTIN\thumbnail-movie-py\spidervswasp.jpeg")
+# Ensure this is the correct path to your video file
+video_path = r"C:\Users\AUSTIN\thumbnail-movie-py\spidervswaspvideo.mp4"
+thumbnail_path = r"C:\Users\AUSTIN\thumbnail-movie-py\spidervswasp.jpeg"
 
-##Set the thumbnail duration
-thumbnail = thumbnail.set_duration(1)
+# Load the video and thumbnail using the correct paths
+video = VideoFileClip(video_path)
+thumbnail = ImageClip(thumbnail_path).set_duration(5).resize(width=video.w).set_start(0)
 
-thumbnail = thumbnail.resize(width=video.w) #aspect ratio
-thumbnail = thumbnail.set_start(0)
+# Combine the thumbnail and video
+final_clip = CompositeVideoClip([thumbnail, video.set_start(thumbnail.duration)])
 
-final_clip = CompositeVideoClip([thumbnail, video.set_start(thumbnail.duration)]) #uses CompositeVideoClip()function that merges multiple clips (video and images) into a single video, layering them according to their start times
-
-final_clip.write_videofile("output_video_with_thumbnail.mp4", codec="libx264") #writes the output into a file
+# Export the final video with the thumbnail
+final_clip.write_videofile("output_video_with_thumbnail.mp4", codec="libx264")
