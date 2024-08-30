cd63ece5502dbbce09076b2fb05c4dc6e7244a75
austin-area61 <austinonyango.area61@gmail.com>
2024-08-30 20:41:15 +0300

    import video and image -set custom thumbnail duration and aspect ratio

--- thumb-nail.py
+++ thumb-nail.py
@@ -1,1 +1,10 @@
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
+
+#import video and image for thumbnail
+video = VideoFileClip("")
+thumbnail = ImageClip("")
+
+##Set the thumbnail duration
+thumbnail = thumbnail.set_duration(1)
+
+thumbnail = thumbnail.resize(width=video.w)
