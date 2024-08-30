dd92c64fe91c1f839150f2432926d87ffd6ffac6
austin-area61 <austinonyango.area61@gmail.com>
2024-08-30 20:52:19 +0300

    added a comment

--- thumb-nail.py
+++ thumb-nail.py
@@ -12,4 +12,4 @@ thumbnail = thumbnail.set_start(0)
 
 final_clip = CompositeVideoClip([thumbnail, video.set_start(thumbnail.duration)]) #uses CompositeVideoClip()function that merges multiple clips (video and images) into a single video, layering them according to their start times
 
-final_clip.write_videofile("output_video_with_thumbnail.mp4", codec="libx264") 
+final_clip.write_videofile("output_video_with_thumbnail.mp4", codec="libx264") #writes the output into a file
