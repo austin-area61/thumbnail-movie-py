f05adb0744eb60e27e35919fdb75bdbd7b6185c8
austin-area61 <austinonyango.area61@gmail.com>
2024-09-01 20:28:19 +0300

    added function to select video file

--- thumbnail_gui.py
+++ thumbnail_gui.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 
+# Function to select video file
+def select_video():
+    video_path = filedialog.askopenfilename(
+        title="Select Video File",
+        filetypes=[("MP4 Files", "*.mp4"), ("All Files", "*.*")]
+    )
+    video_entry.delete(0, tk.END)
+    video_entry.insert(0, video_path)
